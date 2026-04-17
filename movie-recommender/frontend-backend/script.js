428b2e143900af5434710afb746992aa44100c27
ARYISTA <161064447+ARYISTA@users.noreply.github.com>
2026-04-17 08:27:46 +0530

    Delete movie-recommender/frontend-backend/script.js

--- movie-recommender/frontend-backend/script.js
+++
@@ -1,5 +0,0 @@
-function getRecommendations() {
-  let movie = document.getElementById("movie").value;
-  document.getElementById("result").innerHTML =
-    "Recommendations will appear here for <b>" + movie + "</b>";
-}
